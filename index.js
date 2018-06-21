b60b3cd5812ac5004c710bad2d77874cf2e0c45e
Tommy Svensson <tommykajsvensson@gmail.com>
2018-06-21 11:35:34 +0200

    setup middleware, init passport, create user route

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
+const passport = require('passport');
 
 const app = express();
-app.listen(3000, () => {
-  console.log('Listening on port 3000...');
+app.use(morgan('dev')); // logger
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
+
+// routes
+app.use('/users', require('./routes/users'));
+
+const port = process.env.PORT;
+app.listen(port, (err) => {
+  if (err) throw err;
+  console.log(`Listening on port ${port}...`);
 });
